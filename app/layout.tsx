9f2f54a9d539e0adca216ce874a9376369fb3062
ProScriptSlinger <crystal20020123@gmail.com>
2024-04-13 17:50:57 +0900

    fix: add meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,7 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(
-    "https://developer-portfolio-ibrahim-memons-projects.vercel.app"
-  ),
+  metadataBase: new URL("https://valdyslavportfolio.vercel.app/"),
   title: "Vladyslav Stebnitskyi",
   description: "Developer Portfolio By Vladyslav Stebnitskyi",
   keywords: [
